0eac43fd30fdb6d29c77017a5b2f5732fc5a16b0
zm <68761971+szm8991@users.noreply.github.com>
2022-06-06 11:14:11 +0800

    fix: 50day command

--- src/fs.ts
+++ src/fs.ts
@@ -1,16 +1,41 @@
 import { resolve, join } from 'node:path';
 // import { spawn, exec } from 'node:child_process';
 import fs from 'node:fs';
-const fileList50Day = ['index.html', 'style.css', 'script.js'];
+const fileList50Day = [
+  {
+    file: 'index.html',
+    tmp: `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <script src="script.js"></script>
+      </body>
+    </html>`,
+  },
+  {
+    file: 'style.css',
+    tmp: '',
+  },
+  {
+    file: 'script.js',
+    tmp: '',
+  },
+];
 export function create50DayTemplate(folder: string, cwd = process.cwd()): any {
   const isFolderExisted = isPathExisted(folder);
   try {
     if (!isFolderExisted) fs.mkdirSync(folder);
     const path = resolve(cwd, folder);
-    fileList50Day.forEach(file => {
+    fileList50Day.forEach(({ file, tmp }) => {
       const filePath = join(path, file);
       const isFileExisted = fs.existsSync(filePath);
-      if (!isFileExisted) fs.writeFileSync(filePath, '');
+      if (!isFileExisted && file) fs.writeFileSync(filePath, tmp);
     });
   } catch (e) {
     console.warn(`create50DayTemplate failed`);
