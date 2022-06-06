8af5e4e818ff175a92cdc7414a1344e76d663e7f
zm <68761971+szm8991@users.noreply.github.com>
2022-06-06 16:27:32 +0800

    chore: ts-chanllenage creator

---
+++ src/commands/tschan.ts
@@ -0,0 +1,5 @@
+import { createTSCTemplate } from '../fs';
+// 1.接受命令参数——一个参数代表文件夹
+const args = process.argv.slice(2);
+// 2。判断文件夹是否存在
+createTSCTemplate(args[0]);
--- src/fs.ts
+++ src/fs.ts
@@ -20,11 +20,32 @@ const fileList50Day = [
   },
   {
     file: 'style.css',
-    tmp: '',
+    tmp: `* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+}`,
   },
   {
     file: 'script.js',
     tmp: '',
+  },
+];
+const fileTSC = [
+  {
+    file: 'template.ts',
+    tmp: ``,
+  },
+  {
+    file: 'test-cases.ts',
+    tmp: ``,
   },
 ];
 export function create50DayTemplate(folder: string, cwd = process.cwd()): any {
@@ -39,6 +60,21 @@ export function create50DayTemplate(folder: string, cwd = process.cwd()): any {
     });
   } catch (e) {
     console.warn(`create50DayTemplate failed`);
+    process.exit(0);
+  }
+}
+export function createTSCTemplate(folder: string, cwd = process.cwd()): any {
+  const isFolderExisted = isPathExisted(folder);
+  try {
+    if (!isFolderExisted) fs.mkdirSync(folder);
+    const path = resolve(cwd, folder);
+    fileTSC.forEach(({ file, tmp }) => {
+      const filePath = join(path, file);
+      const isFileExisted = fs.existsSync(filePath);
+      if (!isFileExisted && file) fs.writeFileSync(filePath, tmp);
+    });
+  } catch (e) {
+    console.warn(`createTSCTemplate failed`);
     process.exit(0);
   }
 }
